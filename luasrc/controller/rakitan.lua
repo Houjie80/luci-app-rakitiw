424501999cc5972f19be157a86d6aad3ede609ca
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2024-04-16 22:59:15 +0700

    Update rakitan.lua

--- luasrc/controller/rakitan.lua
+++ luasrc/controller/rakitan.lua
@@ -1,4 +1,4 @@
 module("luci.controller.rakitan", package.seeall)
 function index()
-entry({"admin","modem","rakitan"}, template("rakitan"), _("Modem Rakitan"), 7).leaf=true
+entry({"admin","services","rakitan"}, template("rakitan"), _("Modem Rakitan"), 7).leaf=true
 end
